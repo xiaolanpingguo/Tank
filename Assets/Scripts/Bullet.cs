1cf6a1e655e322d29e4aa03ec7ed8f58be66c6e5
xiaolanpingguo <xiaolanpingguo@163.com>
2023-04-16 12:33:26 +0800

    init project

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour 
+{
+    public float moveSpeed = 10;
+    public bool isPlayerBullect;
+
+	// Use this for initialization
+	void Start () 
+    {
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        transform.Translate(transform.up * moveSpeed * Time.deltaTime, Space.World);
+	}
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        switch (collision.tag)
+        {
+            case "Tank":
+                if (!isPlayerBullect)
+                {
+                    collision.SendMessage("Die");
+                    Destroy(gameObject);
+                }
+                break;
+            case "Heart":
+                collision.SendMessage("Die");
+                Destroy(gameObject);
+                break;
+            case "Enemy":
+                if (isPlayerBullect)
+                {
+                    collision.SendMessage("Die");
+                    Destroy(gameObject);
+                }
+                
+                break;
+            case "Wall":
+                Destroy(collision.gameObject);
+                Destroy(gameObject);
+                break;
+            case "Barrier":
+                if (isPlayerBullect)
+                {
+                    collision.SendMessage("PlayAudio");
+                }
+                Destroy(gameObject);
+                break;
+            default:
+                break;
+        }
+    }
+}
---
+++ Assets/Scripts/Explosion.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+        Destroy(gameObject, 0.167f);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+}
